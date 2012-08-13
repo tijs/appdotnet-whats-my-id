651d8af7b26db07d4b4c5fad59804aa44f3fe47c
Tijs Teulings <tijs@automatique.nl>
2012-08-13 21:09:46 +0200

    fixed most issues?

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
 import requests
-import anyjson
-from flask import Flask, session, redirect, url_for, escape, request, render_template
+from flask import Flask, session, redirect, url_for, escape, request, render_template, json, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    if 'access_token' in session:
+        return redirect(url_for('show'))
+
     client_id = os.environ['CLIENT_ID']
-    return render_template('hello.html', client_id=client_id)
+    redirect_url = os.environ['REDIRECT_URL']
+    return render_template('hello.html', client_id=client_id, redirect_url=redirect_url)
 
 @app.route('/oauth/complete')
 def complete():
 
-    code = request.args.get('code', '')
+    code = request.args.get('code', None)
 
     if code:
         # Get the access token here
@@ -21,21 +24,27 @@ def complete():
             'client_id': os.environ['CLIENT_ID'], 
             'client_secret': os.environ['CLIENT_SECRET'],
             'grant_type': 'authorization_code',
-            'redirect_uri': 'http://rocky-sierra-7348.herokuapp.com/oauth/complete',
+            'redirect_uri': os.environ['REDIRECT_URL'],
             'code': code   
         }
         r = requests.post("https://alpha.app.net/oauth/access_token", data=payload)
-        result = anyjson.deserialize(r.text)
-
-        if result.get('error', None):
-            return result.get('error')
+        if r.status_code == requests.codes.ok:
+            result = json.loads(r.text)
+        else:
+            return "sorry but that didn't work"
 
         access_token = result.get('access_token', None)
 
-        if access_token:
-            #save token to session
-            session['access_token'] = access_token
-            redirect(url_for('show'))
+        #save token to session
+        session['access_token'] = access_token
+        return redirect(url_for('show'))
+    
+    return redirect(url_for('hello'))
+
+@app.route('/oauth/logout')
+def logout():
+    if 'access_token' in session:
+        session.pop('access_token', None)
     
     return redirect(url_for('hello'))
 
@@ -44,31 +53,23 @@ def complete():
 def show():
     if 'access_token' in session:
         access_token = escape(session['access_token'])
-        
-        # Get the User details
-        auth_headers = {'Authorization': 'Bearer %s' % access_token }
-        r = requests.get("https://alpha-api.app.net/stream/0/users/me")
-        result = anyjson.deserialize(r.text)
 
-        if result.get('error', None):
-            return result.get('error')
+        # Get the User details
+        headers = {'Authorization': 'Bearer %s' % access_token }
+        r = requests.get("https://alpha-api.app.net/stream/0/users/me", headers=headers)
+        if r.status_code == requests.codes.ok:
+            result = json.loads(r.text)
+        else:
+            return "sorry but that didn't work"
 
         user = result
         return render_template('show.html', user=user)
     else:
         return redirect(url_for('hello'))
 
-
-@app.route('/follow')
-def follow_me():
-    if 'access_token' in session:
-        access_token = escape(session['access_token'])
-        auth_headers = {'Authorization': 'Bearer %s' % access_token }
-        user = requests.post("https://alpha-api.app.net/stream/0/users/346/follow")
-        return redirect(url_for('show'))
-    else:
-        return redirect(url_for('hello'))
-
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -78,7 +79,7 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.secret_key = 'V\x16d|;\x8a\xff]&\x80n\xd7\x98\x01\xd1j\x06,\xa32\x97\xcf_\xfd'
-    app.debug = True
+    #app.debug = True
 
     # setup a simple handler for static files
     app.jinja_env.globals['static'] = (
